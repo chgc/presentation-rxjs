d5663a2d771e9f96da2d584fa20b9db3916f6b58
KevinYang <chgc_kevin@hotmail.com>
2016-08-30 00:42:32 +0800

    modify demo3. add Promise style

--- src/app/demo3/demo3.component.ts
+++ src/app/demo3/demo3.component.ts
@@ -3,24 +3,33 @@ import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/Rx';
 
-@Component({  
+declare var Promise: any;
+
+@Component({
   selector: 'app-demo3',
   templateUrl: 'demo3.component.html',
   styleUrls: ['demo3.component.css']
 })
 export class Demo3Component implements OnInit {
 
-  constructor(private http:Http) { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
     console.clear();
     Observable.forkJoin(
       this.http.get('http://jsonplaceholder.typicode.com/todos').map(res => res.json()),
-      this.http.get('https://jsonplaceholder.typicode.com/users').map(res=>res.json())
+      this.http.get('https://jsonplaceholder.typicode.com/users').map(res => res.json())
     ).subscribe(
       data => console.log(data),
       err => console.error(err.message)
       );
+
+    let p1 = this.http.get('http://jsonplaceholder.typicode.com/todos').map(res => res.json()).toPromise();
+    let p2 = this.http.get('https://jsonplaceholder.typicode.com/users').map(res => res.json()).toPromise();
+
+    Promise.all([p1, p2]).then(values => {
+      console.log('Promise',values);
+    })
   }
 
 }
